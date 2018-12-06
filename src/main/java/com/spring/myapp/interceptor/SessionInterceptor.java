cdde7ac01754ee377c0d429d88237ce66f4e55a2
pc <pc@LAPTOP-V788GDUC>
2018-12-06 11:32:51 +0900

    커밋

--- src/main/java/com/spring/myapp/interceptor/SessionInterceptor.java
+++ src/main/java/com/spring/myapp/interceptor/SessionInterceptor.java
@@ -15,54 +15,12 @@ public class SessionInterceptor extends HandlerInterceptorAdapter {
 	public boolean preHandle(HttpServletRequest request,
 			       HttpServletResponse response, Object handler)
 			throws Exception {
-		//System.out.println("preHandle Call");
-		String reqUrl = request.getRequestURL().toString();
-		System.out.println("reqUrl "+reqUrl);
-		/*
-		System.out.println("11111111111 : "+reqUrl);*/
-		/*System.out.println(session.getAttribute("userid"));
-		if(!reqUrl.equals("http://localhost:8080/") || session.getAttribute("userid")==null){
-			System.out.println("aaa");
-			return true;
-		}
 		
-		else {
-			System.out.println("bbbbb");
-			response.sendRedirect("/");
-			return true;
-			
-		}
 		if(session.getAttribute("userid")==null){
-			System.out.println("1222222222222222222222222222222222");
 			response.sendRedirect("/");
 			return false;
 		}
 		return true;
-		
-		*/
-		//!reqUrl.equals("http://localhost:8080/")
-		System.out.println(session.getAttribute("userid"));
-		
-		if(session.getAttribute("userid") != null) {
-			response.sendRedirect("/board/list");
-			return false;
-		}else {
-			if(reqUrl.equals("http://localhost:8080/") || reqUrl.equals("http://localhost:8080/login") || reqUrl.equals("http://localhost:8080/logout") ) {
-				return true;	
-			} else if(reqUrl.equals("http://localhost:8080/joinform") || reqUrl.equals("http://localhost:8080/idcheck") || reqUrl.equals("http://localhost:8080/join")) {
-				return true;	
-			}else {
-				if(session.getAttribute("userid")==null){
-					System.out.println("aaa");
-					response.sendRedirect("/");
-					return false;
-				}
-				return true;
-			}
-		}
-		
-		
-		
 	}
 	
 }
